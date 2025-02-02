69195eee66b27fb899c3324b909a30239bff7e1c
Miramaged802 <miramaged345@gmail.com>
2025-02-02 11:46:37 +0200

    first

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let menu = document.querySelector("#menu-icons");
+let navbar = document.querySelector(".nav-links");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+const container = document.querySelector(".container");
+const registerBtn = document.querySelector(".register-btn");
+const loginBtn = document.querySelector(".login-btn");
+
+registerBtn.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+loginBtn.addEventListener("click", () => {
+  container.classList.remove("active");
+});
+
